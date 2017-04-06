aa2aba485918e91aee584995c5d854fc570b2578
Zeks1990 <zhaoxu942698@163.com>
2017-04-06 11:57:47 +0800

    增加判断输赢

--- js/script.js
+++ js/script.js
@@ -33,6 +33,66 @@ var me=true;
 //声明棋盘记录以及步数
 var chessBoardRecord=[];
 var chessStep=0;
+//声明是否结束游戏
+var over=false;
+
+//赢法数组
+var wins=[];
+for(var i=0;i<15;i++){
+	wins[i]=[];
+	for(var j=0;j<15;j++){
+		wins[i][j]=[];
+	}
+}
+//赢法种类索引
+var count=0;
+//填充赢法数组
+//横线判定
+for(var i=0;i<15;i++){
+	for(var j=0;j<11;j++){
+		for(var k=0;k<5;k++){
+			wins[i][j+k][count]=true;
+		}
+		count++;
+	}
+}
+//竖线判定
+for(var i=0;i<15;i++){
+	for(var j=0;j<11;j++){
+		for(var k=0;k<5;k++){
+			wins[j+k][i][count]=true;
+		}
+		count++;
+	}
+}
+//正斜线判定
+for(var i=0;i<11;i++){
+	for(var j=0;j<11;j++){
+		for(var k=0;k<5;k++){
+			wins[i+k][j+k][count]=true;
+		}
+		count++;
+	}
+}
+//反斜线判定
+for(var i=0;i<11;i++){
+	for(var j=14;j>3;j--){
+		for(var k=0;k<5;k++){
+			wins[i+k][j-k][count]=true;
+		}
+		count++;
+	}
+}
+
+//赢法统计数组
+var myWin=[];
+var matchWin=[];
+//初始化赢法统计数组
+for(var i=0;i<count;i++){
+	myWin[i]=0;
+	matchWin[i]=0;
+}
+
 
 var oneStep=function(i,j,me){
 	//画棋子
@@ -54,6 +114,9 @@ var oneStep=function(i,j,me){
 }
 
 chess.onclick=function(e){
+	if(over){
+		return;
+	}
 	var x=e.offsetX;
 	var y=e.offsetY;
 	console.log(x+","+y);
@@ -70,6 +133,18 @@ chess.onclick=function(e){
 		//记录走过的棋子
 		chessBoardRecord.push([i,j]);
 		console.log(i+","+j);
+		for(var k=0;k<count;k++){
+			if(wins[i][j][k]){
+				myWin[k]++;
+				matchWin[k]=9;
+				if(myWin[k]==5){
+					setTimeout(function(){
+						window.alert("你赢了！");
+					},0);
+					over=true;
+				}
+			}
+		}
 	}
 }
 
@@ -86,7 +161,6 @@ withdraw.onclick=function(){
 			context.clearRect(recordX-15,recordY-15,30,30);
 			context.beginPath();
 			context.closePath();
-			//context.fillStyle="";
 			//横线
 			context.moveTo(record[0]==0?recordX:recordX-15,recordY);
 			context.lineTo(record[0]==14?recordX:recordX+15,recordY);
