a7259ca9e28413fa08e4f4462fc959c26aa364ca
Zeks1990 <zhaoxu942698@163.com>
2017-04-10 17:34:54 +0800

    添加功能，优化

--- js/script.js
+++ js/script.js
@@ -35,6 +35,10 @@ var chessBoardRecord=[];
 var chessStep=0;
 //声明是否结束游戏
 var over=false;
+//声明游戏状态
+const MATCH=1;
+const AI=2;
+var status=MATCH;
 
 //赢法数组
 var wins=[];
@@ -112,7 +116,39 @@ var oneStep=function(i,j,me){
 	context.fillStyle=gradient;
 	context.fill();
 	chessStep++;
-	console.log(chessStep);
+	//console.log(chessStep);
+}
+//封装赢法统计数据算法
+var winFun=function(i,j){
+	//记录走过的棋子
+	chessBoardRecord.push([i,j]);
+	var arr=[];
+	for(var k=0;k<count;k++){
+		if(wins[i][j][k]){
+			if(me){
+				myWin[k]++;
+				matchWin[k]+=9;
+			}else{
+				myWin[k]+=9;
+				matchWin[k]++;
+			}
+			if(myWin[k]==5){
+				setTimeout(function(){
+					window.alert("黑棋胜！");
+				},0);
+				over=true;
+			}else if(matchWin[k]==5){
+				setTimeout(function(){
+					window.alert("白棋胜！");
+				},0);
+				over=true;
+			}
+			arr.push(k);
+		}
+	}
+	//console.log(arr);
+	winsRecord.push(arr);
+	//console.log(winsRecord);
 }
 
 chess.onclick=function(e){
@@ -121,87 +157,153 @@ chess.onclick=function(e){
 	}
 	var x=e.offsetX;
 	var y=e.offsetY;
-	console.log(x+","+y);
+	//console.log(x+","+y);
 	var i=Math.floor(x/30);
 	var j=Math.floor(y/30);
 	if(chessBoard[i][j]==0){
 		oneStep(i,j,me);
 		if(me){
 			chessBoard[i][j]=1;
-		}else{
+		}else if(status==1){
 			chessBoard[i][j]=2;
 		}
-		//记录走过的棋子
-		chessBoardRecord.push([i,j]);
-		//console.log(i+","+j);
-		var arr=[];
-		for(var k=0;k<count;k++){
-			if(wins[i][j][k]){
-				if(me){
-					myWin[k]++;
-					matchWin[k]+=9;
-				}else{
-					myWin[k]+=9;
-					matchWin[k]++;
-				}
-				if(myWin[k]==5){
-					setTimeout(function(){
-						window.alert("黑棋胜！");
-					},0);
-					over=true;
-				}else if(matchWin[k]==5){
-					setTimeout(function(){
-						window.alert("白棋胜！");
-					},0);
-					over=true;
-				}
-				arr.push(k);
-			}
-		}
-		//console.log(arr);
-		winsRecord.push(arr);
-		//console.log(winsRecord);
+		winFun(i,j);
 		me=!me;
+		if(!over&&status==2){
+			computerAI();
+		}
 	}
 }
 
+var computerAI=function(){
+	var myScore=[];
+	var computerScore=[];
+	var max=0;
+	var u=0,v=0;
+	for(var i=0;i<15;i++){
+		myScore[i]=[];
+		computerScore[i]=[];
+		for(var j=0;j<15;j++){
+			myScore[i][j]=0;
+			computerScore[i][j]=0;
+		}
+	}
+	for(var i=0;i<15;i++){
+		for(var j=0;j<15;j++){
+			if(chessBoard[i][j]==0){
+				for(var k=0;k<count;k++){
+					if(wins[i][j][k]){
+						if(myWin[k]==1){
+							myScore[i][j]+=100;
+						}else if(myWin[k]==2){
+							myScore[i][j]+=400;
+						}else if(myWin[k]==3){
+							myScore[i][j]+=2000;
+						}else if(myWin[k]==4){
+							myScore[i][j]+=22000;
+						}
+						if( matchWin[k]==1){
+							computerScore[i][j]+=110;
+						}else if(matchWin[k]==2){
+							computerScore[i][j]+=440;
+						}else if(matchWin[k]==3){
+							computerScore[i][j]+=2200;
+						}else if(matchWin[k]==4){
+							computerScore[i][j]+=23200;
+						}
+					}
+				}
+				if(myScore[i][j]>max){
+					max=myScore[i][j];
+					u=i;
+					v=j;
+				}else if(myScore[i][j]==max){
+					if(computerScore[i][j]>computerScore[u][v]){
+						u=i;
+						v=j;
+					}
+				}
+				if(computerScore[i][j]>max){
+					max=computerScore[i][j];
+					u=i;
+					v=j;
+				}else if(computerScore[i][j]==max){
+					if(myScore[i][j]>myScore[u][v]){
+						u=i;
+						v=j;
+					}
+				}
+			}
+		}
+	}
+	oneStep(u,v,false);
+	chessBoard[u][v]=2;
+	winFun(u,v);
+	me=!me;
+}
 
-/*自创*/
+/***********自创*************/
 //悔棋
 var withdraw=document.getElementById('withdraw');
+var withdrawFun=function(){
+	var record=chessBoardRecord.pop();
+	var recordX=record[0]*30+15;
+	var recordY=record[1]*30+15;
+	context.clearRect(recordX-15,recordY-15,30,30);
+	context.beginPath();
+	context.closePath();
+	//横线
+	context.moveTo(record[0]==0?recordX:recordX-15,recordY);
+	context.lineTo(record[0]==14?recordX:recordX+15,recordY);
+	//竖线
+	context.moveTo(recordX,record[1]==0?recordY:recordY-15);
+	context.lineTo(recordX,record[1]==14?recordY:recordY+15);
+	context.stroke();
+	chessBoard[record[0]][record[1]]=0;
+	chessStep--;
+	me=!me;
+	var withdrawWinsRecord=winsRecord.pop();
+	console.log(withdrawWinsRecord);
+	if(me){
+		for(var i=0;i<withdrawWinsRecord.length;i++){
+			myWin[withdrawWinsRecord[i]]--;
+			matchWin[withdrawWinsRecord[i]]-=9;
+		}
+	}else{
+		for(var i=0;i<withdrawWinsRecord.length;i++){
+			myWin[withdrawWinsRecord[i]]-=9;
+			matchWin[withdrawWinsRecord[i]]--;
+		}
+	}
+}
 withdraw.onclick=function(){
-	if(chessStep>=1){
-		var record=chessBoardRecord.pop();
-		var recordX=record[0]*30+15;
-		var recordY=record[1]*30+15;
-		context.clearRect(recordX-15,recordY-15,30,30);
-		context.beginPath();
-		context.closePath();
-		//横线
-		context.moveTo(record[0]==0?recordX:recordX-15,recordY);
-		context.lineTo(record[0]==14?recordX:recordX+15,recordY);
-		//竖线
-		context.moveTo(recordX,record[1]==0?recordY:recordY-15);
-		context.lineTo(recordX,record[1]==14?recordY:recordY+15);
-		context.stroke();
-		chessBoard[record[0]][record[1]]=0;
-		chessStep--;
-		over=false;
-		me=!me;
-		var withdrawWinsRecord=winsRecord.pop();
-		console.log(withdrawWinsRecord);
-		if(me){
-			for(var i=0;i<withdrawWinsRecord.length;i++){
-				myWin[withdrawWinsRecord[i]]--;
-				matchWin[withdrawWinsRecord[i]]-=9;
-			}
-		}else{
-			for(var i=0;i<withdrawWinsRecord.length;i++){
-				myWin[withdrawWinsRecord[i]]-=9;
-				matchWin[withdrawWinsRecord[i]]--;
-			}
+	if(chessStep>=1&&over==false){
+		withdrawFun();
+		if(status==2){
+			withdrawFun();
 		}
 	}else{
 		alert('无法悔棋了~');
 	}
+}
+//模式切换
+var changeMode=document.getElementById('changeMode');
+changeMode.onclick=function(){
+	if(chessStep==0){
+		if(status==1){
+			status=AI;
+			document.getElementById('mode').innerHTML='人机对战';
+		}else{
+			status=1;
+			document.getElementById('mode').innerHTML='玩家对战';
+		}
+	}else{
+		alert('请重新开局或悔棋到最后再选择模式~');
+	}
+}
+//重新开局
+var restart=document.getElementById('restart');
+restart.onclick=function(){
+	//drawChessBoard();
+	location.reload();
 }
